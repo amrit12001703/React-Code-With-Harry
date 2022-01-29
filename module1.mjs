6ea9469506febf2271bfad9d54c6af3277c0acd8
amrit12001703 <amrit.12001703@lpu.in>
2022-01-29 12:17:25 +0530

    create components

--- module1.mjs
+++
@@ -1,7 +0,0 @@
-const a="Amrit";
-const b="Sakchi";
-const c="Akash";
-
-export default a;
-export{b};
-export{c};
--- module2.mjs
+++
@@ -1,4 +0,0 @@
-import x, {b,c} from "./module1.mjs";
-console.log(x);
-console.log(b);
-console.log(c);
