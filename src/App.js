c29a25c7aad57a8c145f811cef4e2a423dbb8f70
amrit12001703 <amrit.12001703@lpu.in>
2022-02-09 22:36:22 +0530

    Router Intro

--- src/App.js
+++ src/App.js
@@ -1,50 +1,56 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
-import React, {useState} from 'react';
-import Alert from './components/Alert';
-// import About from './components/About';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import React, { useState } from "react";
+import Alert from "./components/Alert";
+import About from "./components/About";
+
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
-  const[mode, setMode]=useState('light');
- const [alert, setAlert] = useState(null);
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
 
- const showAlert=(message,type)=>{
-        setAlert({
-          msg:message,
-          type:type,
-        })
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
 
-        setTimeout(()=>{
-          setAlert(null);
-        },1500);
- }
-  
-  const toggleMode=()=>{
-    if(mode ==='light'){
-      setMode('dark');
-      document.body.style.backgroundColor='#042743';
-      showAlert("Dark Mode Has Been Enabled","success")
-      document.title="TextUtil-Dark";
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark Mode Has Been Enabled", "success");
+      document.title = "TextUtil-Dark";
+    } else if (mode === "dark") {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light Mode Has Been Enabled", "success");
+      document.title = "TextUtil-Light";
     }
-    else if(mode==='dark'){
-      setMode('light');
-      document.body.style.backgroundColor='white';
-      showAlert("Light Mode Has Been Enabled","success");
-      document.title="TextUtil-Light";
-    }
-  }
+  };
 
   return (
     <>
-     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
-     <Alert alert={alert} /> 
-     <div className='container my-3'>
-     <TextForm  
-    showAlert={showAlert} heading="Enter the text to analyse below" mode={mode}/> 
-     {/* <About/> */}
-     </div>
-   </>
+      <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} aboutText="About"/>
+        <Alert alert={alert} />
+        <div className="container my-3">
+        <Routes>
+            <Route path="/about" element={<About />}>
+            </Route>
+            <Route path="/" element={<TextForm heading="Enter text to analyze"  mode={mode} showAlert={showAlert} />}>
+            </Route>
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
